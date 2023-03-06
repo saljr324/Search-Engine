87e81a3a3a607d0984241e1ab6b35295341b0f33
saljr324 <68786450+saljr324@users.noreply.github.com>
2023-03-06 13:06:23 -0800

    Update Search_Repo_Test.py

--- Streamlit/Search_Repo_Test.py
+++ Streamlit/Search_Repo_Test.py
@@ -67,87 +67,87 @@ truth = []
 raw_contents = []
 unique_repos = []
 
-if title:
+# if title:
 
-    for i in range(len(first35_repos)):
-        for j in range(len(first35_repos[i])):
-            check = first35_repos[i][j][0]
-            if type(check) == str:
-                if title in check.lower():
-                    contentfile = eval(filepaths[i])[j]
-                    truth.append([i, j])
-                    if i not in unique_repos:
-                        unique_repos.append(i)
-                    raw_contents.append(check.lower())
+for i in range(len(first35_repos)):
+    for j in range(len(first35_repos[i])):
+        check = first35_repos[i][j][0]
+        if type(check) == str:
+            if title in check.lower():
+                contentfile = eval(filepaths[i])[j]
+                truth.append([i, j])
+                if i not in unique_repos:
+                    unique_repos.append(i)
+                raw_contents.append(check.lower())
 
-    for i in range(len(second35_repos)):
-        for j in range(len(second35_repos[i])):
-            check = second35_repos[i][j][0]
-            if type(check) == str:
-                if title in check.lower():
-                    contentfile = eval(filepaths[35 + i])[j]
-                    truth.append([35 + i, j])
-                    if 35 + i not in unique_repos:
-                        unique_repos.append(35 + i)
-                    raw_contents.append(check.lower())
+for i in range(len(second35_repos)):
+    for j in range(len(second35_repos[i])):
+        check = second35_repos[i][j][0]
+        if type(check) == str:
+            if title in check.lower():
+                contentfile = eval(filepaths[35 + i])[j]
+                truth.append([35 + i, j])
+                if 35 + i not in unique_repos:
+                    unique_repos.append(35 + i)
+                raw_contents.append(check.lower())
 
-    for i in range(len(third35_repos)):
-        for j in range(len(third35_repos[i])):
-            check = third35_repos[i][j][0]
-            if type(check) == str:
-                if title in check.lower():
-                    contentfile = eval(filepaths[70 + i])[j]
-                    truth.append([70 + i, j])
-                    if 70 + i not in unique_repos:
-                        unique_repos.append(70 + i)
-                    raw_contents.append(check.lower())
-                    
-    for i in range(len(fourth35_repos)):
-        for j in range(len(fourth35_repos[i])):
-            check = fourth35_repos[i][j][0]
-            if type(check) == str:
-                if title in check.lower():
-                    contentfile = eval(filepaths[105 + i])[j]
-                    truth.append([105 + i, j])
-                    if 105 + i not in unique_repos:
-                        unique_repos.append(105 + i)
-                    raw_contents.append(check.lower())
-        
-    for i in range(len(fifth23_repos)):
-        for j in range(len(fifth23_repos[i])):
-            check = fifth23_repos[i][j][0]
-            if type(check) == str:
-                if title in check.lower():
-                    print(140+i,j)
-                    contentfile = eval(filepaths[140 + i])[j]
-                    truth.append([140 + i, j])
-                    if 140 + i not in unique_repos:
-                        unique_repos.append(140 + i)
-                    raw_contents.append(check.lower())
+for i in range(len(third35_repos)):
+    for j in range(len(third35_repos[i])):
+        check = third35_repos[i][j][0]
+        if type(check) == str:
+            if title in check.lower():
+                contentfile = eval(filepaths[70 + i])[j]
+                truth.append([70 + i, j])
+                if 70 + i not in unique_repos:
+                    unique_repos.append(70 + i)
+                raw_contents.append(check.lower())
 
-    repo_names = [repo_list[i] for i in unique_repos]
-    repo_tabs = [re.findall(r'\/(.+)', i.full_name)[0] for i in repo_names] 
-    truth.reverse()
-    repo_tabs.reverse()
-    if len(truth) == 0:
-            st.write('None Found')
-    else:
-        tabs = st.tabs(repo_tabs)
-        current_repo = repo_tabs[0]
-        count = 0
+for i in range(len(fourth35_repos)):
+    for j in range(len(fourth35_repos[i])):
+        check = fourth35_repos[i][j][0]
+        if type(check) == str:
+            if title in check.lower():
+                contentfile = eval(filepaths[105 + i])[j]
+                truth.append([105 + i, j])
+                if 105 + i not in unique_repos:
+                    unique_repos.append(105 + i)
+                raw_contents.append(check.lower())
 
-        for i in range(len(truth)):
-            repo = truth[i][0]
-            file = truth[i][1]
-            contentfile = eval(filepaths[repo])[file]
-            path = re.findall('=.*', contentfile)[0][2:-2]
+for i in range(len(fifth23_repos)):
+    for j in range(len(fifth23_repos[i])):
+        check = fifth23_repos[i][j][0]
+        if type(check) == str:
+            if title in check.lower():
+                print(140+i,j)
+                contentfile = eval(filepaths[140 + i])[j]
+                truth.append([140 + i, j])
+                if 140 + i not in unique_repos:
+                    unique_repos.append(140 + i)
+                raw_contents.append(check.lower())
 
-            url = repo_list[repo].get_contents(path = path).html_url
-            clean_url = re.findall(r'(?:/)(.*?)(?:.)', url)
-            repo = re.search(r'ds-modules/(.*?)/', url).group(1)
-            if repo != current_repo:
-                count += 1
-                current_repo = repo_tabs[count]
-            with tabs[count]:
-                st.write(url)
-        st.write('Complete')
+repo_names = [repo_list[i] for i in unique_repos]
+repo_tabs = [re.findall(r'\/(.+)', i.full_name)[0] for i in repo_names] 
+truth.reverse()
+repo_tabs.reverse()
+if len(truth) == 0:
+        st.write('None Found')
+else:
+    tabs = st.tabs(repo_tabs)
+    current_repo = repo_tabs[0]
+    count = 0
+
+    for i in range(len(truth)):
+        repo = truth[i][0]
+        file = truth[i][1]
+        contentfile = eval(filepaths[repo])[file]
+        path = re.findall('=.*', contentfile)[0][2:-2]
+
+        url = repo_list[repo].get_contents(path = path).html_url
+        clean_url = re.findall(r'(?:/)(.*?)(?:.)', url)
+        repo = re.search(r'ds-modules/(.*?)/', url).group(1)
+        if repo != current_repo:
+            count += 1
+            current_repo = repo_tabs[count]
+        with tabs[count]:
+            st.write(url)
+    st.write('Complete')
