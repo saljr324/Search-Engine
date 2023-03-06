0dd9ea4dbfaaa03bdb75aa18bd2f5a3857644c8b
saljr324 <68786450+saljr324@users.noreply.github.com>
2023-03-06 12:55:59 -0800

    Update Search_Repo_Test.py

--- Streamlit/Search_Repo_Test.py
+++ Streamlit/Search_Repo_Test.py
@@ -106,22 +106,22 @@ import ast
 
 @st.cache_resource
 def openfiles():
-    with open('first35.txt', 'r') as f:
+    with open('Streamlit/first35.txt', 'r') as f:
         first35_repos = [list(ast.literal_eval(line)) for line in f]
 
-    with open('second35.txt', 'r') as f:
+    with open('Streamlit/second35.txt', 'r') as f:
         second35_repos = [list(ast.literal_eval(line)) for line in f]
 
-    with open('third35.txt', 'r') as f:
+    with open('Streamlit/third35.txt', 'r') as f:
         third35_repos = [list(ast.literal_eval(line)) for line in f]
 
-    with open('fourth35.txt', 'r') as f:
+    with open('Streamlit/fourth35.txt', 'r') as f:
         fourth35_repos = [list(ast.literal_eval(line)) for line in f]
 
-    with open('fifth23.txt', 'r') as f:
+    with open('Streamlit/fifth23.txt', 'r') as f:
         fifth23_repos = [list(ast.literal_eval(line)) for line in f]
 
-    with open('filepaths.txt', 'r') as f:
+    with open('Streamlit/filepaths.txt', 'r') as f:
         filepaths = [line for line in f]
 
     return first35_repos, second35_repos, third35_repos, fourth35_repos, fifth23_repos, filepaths
