108d51aaa48833113a27df5da2794927b53e4bb4
saljr324 <68786450+saljr324@users.noreply.github.com>
2023-03-06 13:03:58 -0800

    Update Search_Repo_Test.py

--- Streamlit/Search_Repo_Test.py
+++ Streamlit/Search_Repo_Test.py
@@ -10,9 +10,6 @@ from difflib import SequenceMatcher
 import os
 
 
-# token = ''
-# client = Github()
-
 client = Github('cec181f6847eb6472425', '1ab6e2c3b3ed83f4b25ecd390cd5ef65c629b591')
 
 @st.cache_resource
@@ -31,76 +28,6 @@ def get_repos():
     return repo_list
 
 repo_list = get_repos()
-
-def search_github(keyword, query):
-    '''
-    keyword: what you want to search (query)
-    query: https://docs.github.com/en/search-github/searching-on-github/searching-code#search-within-a-users-or-organizations-repositories
-    '''
-    rate_limit = client.get_rate_limit()
-    rate = rate_limit.search
-    if rate.remaining == 0:
-        print(f'You have 0/{rate.limit} API calls remaining. Reset time: {rate.reset}')
-        return
-    else:
-        print(f'You have {rate.remaining}/{rate.limit} API calls remaining')
-
-    # to change query: add/remove parameters
-    # useful extensions: md, ipynb, etc...
-    # visit website linked in description
-    print('Query: ' + keyword + ' ' + query)
-    result = client.search_code(keyword + ' ' + query)
-
-    max_size = 100
-    print(f'Found {result.totalCount} file(s)')
-    if result.totalCount > max_size:
-        result = result[:max_size]
-
-    for file in result:
-        print(f'{file.download_url}')
-
-    st.write(result)
-
-    return result
-
-@st.cache_data
-def get_all_contents():
-    # NOTICE: unless you want to go over your allowed API calls per hour, only run this once.
-    total_modules_ipynb = 0
-    ipynb_counts = []
-    filepaths = []
-    for i in range(len(repo_list)):
-        rate_limit = client.get_rate_limit()
-        rate = rate_limit.search
-        if rate.remaining == 0:
-            print('loading API...')
-            time.sleep(20)
-        else:
-            print(f'You have {rate.remaining}/{rate.limit} API calls remaining')
-
-        test_repo = client.get_repo(repo_list[i].full_name)
-        print('Repo Name: ', test_repo,)
-        print('Repo Link: ', f'github.com/{test_repo.full_name}')
-
-        total_ipynb = 0
-        test_contents = test_repo.get_contents("")
-        file_contents = []
-
-        while test_contents:
-            file_content = test_contents.pop(0)
-            if file_content.type == "dir":
-                test_contents.extend(test_repo.get_contents(file_content.path))
-            else:
-                if 'ipynb' in file_content.path:
-                    total_ipynb += 1
-                file_contents.append(file_content)
-        filepaths.append(file_contents)
-
-        total_modules_ipynb += total_ipynb
-        ipynb_counts.append(total_ipynb)
-        print('Total .ipynb files in this repo: ', total_ipynb)
-        print('Total .ipynb files in ds-modules: ', total_modules_ipynb)
-        print('---')
 
 import ast
 
